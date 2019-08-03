a05c4f1053f380110e5a8833336118f603ee4d87
thegibson <yukenquiries@gmail.com>
2019-08-03 16:58:16 +0100

    about toooo uinstert 3d onbz

--- public/script.js
+++ public/script.js
@@ -5,11 +5,15 @@ console.log('AVATAR SELECTA');
 //     return true;
 // }
 
+// document.body.addEventListener('click', e => console.log(e.target));
+
 if (location.protocol != 'http:') {
     location.href = 'http:' + window.location.href.substring(window.location.protocol.length);
 }
 
-var uVizAd = document.getElementById('capita'); 
+var uVizAd = document.getElementById('capita');
+var avatarHeads = document.getElementsByClassName('avatarHead');
+var avatarName = document.getElementById('avatarName');
 
 var socket = io.connect('http://192.168.1.226:8080'); //studio
 
@@ -27,3 +31,22 @@ socket.on('connect', function(data) {
        }, 6000);
    });
 });
+
+for (var i = 0; i < avatarHeads.length; i++) {
+    avatarHeads[i].addEventListener('click', e => {
+        changeAvatar(e.target.alt)
+    });
+}
+
+var currentlySelected = {
+    name: 'james',
+    index: 0
+};
+
+function changeAvatar(who) {
+    var index = who === 'james' ? 0 : who === 'grant' ? 1 : who === 'tori' ? 2 : 3;
+    avatarName.innerText = who;
+    avatarHeads[currentlySelected.index].classList.remove('selectedHead');
+    avatarHeads[index].classList.add('selectedHead');
+    currentlySelected = {name: who, index: index};
+}
