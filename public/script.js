764d355dda086eb1e9c2faa1c19bcb0f3eba073d
thegibson <yukenquiries@gmail.com>
2019-08-05 17:28:12 +0100

    loading screen

--- public/script.js
+++ public/script.js
@@ -7,16 +7,25 @@ console.log('AVATAR SELECTA');
 
 // document.body.addEventListener('click', e => console.log(e.target));
 
+
+// ELEMENTS ===============================================================
+
 if (location.protocol != 'http:') {
     location.href = 'http:' + window.location.href.substring(window.location.protocol.length);
 }
 
 var uVizAd = document.getElementById('capita');
+var avatarHeadsWrap = document.getElementById('avatarHeads');
 var avatarHeads = document.getElementsByClassName('avatarHead');
 var avatarName = document.getElementById('avatarName');
+var initializing = document.getElementById('initializing');
+var terminal = document.getElementById('terminal');
 
-// var socket = io.connect('http://192.168.1.226:8080'); //studio
-var socket = io.connect('http://192.168.4.1:8080'); //ultraPi
+
+// SOCKETS ===============================================================
+
+var socket = io.connect('http://192.168.1.226:8080'); //studio
+// var socket = io.connect('http://192.168.4.1:8080'); //ultraPi
 
 socket.on('connect', function(data) {
    socket.emit('join', {who: 'selecta'});
@@ -33,11 +42,140 @@ socket.on('connect', function(data) {
    });
 });
 
+
+// THREE JS =================================================================
+
+var scene = new THREE.Scene();
+
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.z = 2.5;
+
+var renderer = new THREE.WebGLRenderer({antialias:true, alpha: true});
+document.body.appendChild( renderer.domElement );
+var modelCanv = document.getElementsByTagName('canvas')[0];
+renderer.setClearColor("#fff", 0);
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+// renderer.gammaOutput = true;
+// renderer.gammaFactor = 5.3;
+
+var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.8 );
+directionalLight.position.set(1, 2, 5);
+scene.add( directionalLight );
+
+var james, grant, tori, ian;
+var assetCount = 0;
+
+function assetLoaded() {
+    assetCount++;
+    console.log('asset count' ,assetCount);
+    if (assetCount === 4) {
+        scene.add(james);
+        james.position.set(-0.3, -0.7, 0);
+        terminal.style.display = 'none';
+        modelCanv.style.display = 'block';
+        avatarName.style.display = 'block';
+        avatarHeadsWrap.style.display = 'block';
+        uVizAd.style.display = 'flex';
+        initializing.innerText = 'choose a DANK member';
+    }
+}
+
+var loader = new THREE.OBJLoader();
+
+loader.load(
+	`assets/avatarSelecta/jModel.obj`,
+	function (object) {
+        james = object;
+        assetLoaded();
+	},
+	function (xhr) {
+		console.log( (xhr.loaded / xhr.total) + ' loaded james');
+        terminal.innerHTML = terminal.innerHTML + `<p>${xhr.loaded / xhr.total}</p>`
+	},
+	function (error) {
+		console.log( 'An error happened' );
+	}
+);
+
+loader.load(
+	`assets/avatarSelecta/gModelZ.obj`,
+	function (object) {
+        grant = object;
+        assetLoaded();
+	},
+	function (xhr) {
+		console.log( (xhr.loaded / xhr.total) + ' loaded grant');
+        terminal.innerHTML = terminal.innerHTML + `<p>${xhr.loaded / xhr.total}</p>`
+	},
+	function (error) {
+		console.log( 'An error happened' );
+	}
+);
+
+loader.load(
+	`assets/avatarSelecta/tModel.obj`,
+	function (object) {
+        tori = object;
+        assetLoaded();
+	},
+	function (xhr) {
+		console.log( (xhr.loaded / xhr.total) + ' loaded tori');
+        terminal.innerHTML = terminal.innerHTML + `<p>${xhr.loaded / xhr.total}</p>`
+	},
+	function (error) {
+		console.log( 'An error happened' );
+	}
+);
+
+loader.load(
+	`assets/avatarSelecta/iModel.obj`,
+	function (object) {
+        ian = object;
+        assetLoaded();
+	},
+	function (xhr) {
+		console.log( (xhr.loaded / xhr.total) + ' loaded ian');
+        terminal.innerHTML = terminal.innerHTML + `<p>${xhr.loaded / xhr.total}</p>`
+	},
+	function (error) {
+		console.log( 'An error happened' );
+	}
+);
+
+var render = function () {
+    requestAnimationFrame( render );
+
+    if (james) {
+        james.rotation.y += 0.01;
+    }
+    if (grant) {
+        grant.rotation.y += 0.01;
+    }
+    if (tori) {
+        tori.rotation.y += 0.01;
+    }
+    if (ian) {
+        ian.rotation.y += 0.01;
+    }
+
+    renderer.render(scene, camera);
+};
+
+render();
+
+
+// THE REST ================================================================
+
 for (var i = 0; i < avatarHeads.length; i++) {
     avatarHeads[i].addEventListener('click', e => {
-        changeAvatar(e.target.alt)
+        changeAvatar(e.target.innerText)
     });
 }
+
+modelCanv.addEventListener('click', () => {
+    enterStream();
+})
 
 var currentlySelected = {
     name: 'james',
@@ -46,7 +184,7 @@ var currentlySelected = {
 
 function changeAvatar(who) {
     var index = who === 'james' ? 0 : who === 'grant' ? 1 : who === 'tori' ? 2 : 3;
-    avatarName.innerText = who;
+    avatarName.innerText = `< ${who} >`;
     avatarHeads[currentlySelected.index].classList.remove('selectedHead');
     avatarHeads[index].classList.add('selectedHead');
     new Promise((resolve, reject) => {
@@ -88,99 +226,7 @@ function changeAvatar(who) {
     });
 }
 
-
-var scene = new THREE.Scene();
-
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.z = 2.5;
-
-var renderer = new THREE.WebGLRenderer({antialias:true, alpha: true});
-document.body.appendChild( renderer.domElement );
-renderer.setClearColor("#fff", 0);
-
-renderer.setSize( window.innerWidth, window.innerHeight );
-// renderer.gammaOutput = true;
-// renderer.gammaFactor = 5.3;
-
-var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.8 );
-directionalLight.position.set(1, 2, 5);
-scene.add( directionalLight );
-
-var james, grant, tori, ian;
-
-var loader = new THREE.OBJLoader();
-
-loader.load(
-	`assets/avatarSelecta/jModel.obj`,
-	function (object) {
-        james = object;
-		scene.add(james);
-        james.position.set(-0.3, -0.7, 0);
-	},
-	function (xhr) {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-	},
-	function (error) {
-		console.log( 'An error happened' );
-	}
-);
-
-loader.load(
-	`assets/avatarSelecta/gModelZ.obj`,
-	function (object) {
-        grant = object;
-	},
-	function (xhr) {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-	},
-	function (error) {
-		console.log( 'An error happened' );
-	}
-);
-
-loader.load(
-	`assets/avatarSelecta/tModel.obj`,
-	function (object) {
-        tori = object;
-	},
-	function (xhr) {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-	},
-	function (error) {
-		console.log( 'An error happened' );
-	}
-);
-
-loader.load(
-	`assets/avatarSelecta/iModel.obj`,
-	function (object) {
-        ian = object;
-	},
-	function (xhr) {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-	},
-	function (error) {
-		console.log( 'An error happened' );
-	}
-);
-
-var render = function () {
-    requestAnimationFrame( render );
-
-    if (james) {
-        james.rotation.y += 0.01;
-    }
-    if (grant) {
-        grant.rotation.y += 0.01;
-    }
-    if (tori) {
-        tori.rotation.y += 0.01;
-    }
-    if (ian) {
-        ian.rotation.y += 0.01;
-    }
-
-    renderer.render(scene, camera);
-};
-
-render();
+function enterStream() {
+    console.log(`entering ${currentlySelected.name}'s stream!'`);
+    window.location.href = `http://192.168.1.226:8080/${currentlySelected.name}`;
+}
